90056dcbfad5500c07a410dfe5b20338fff1d224
lorenzotarragoni <tarragonilorenzo@gmail.com>
2025-12-09 15:54:40 +0100

    add HTML form and PHP script for student data insertion

---
+++ 2-PHP/Insert_Attributes_Database/insert.php
@@ -0,0 +1,30 @@
+<?php
+$conn = new mysqli('localhost', 'root', '', 'gestione_scuola');
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$dob = $_POST['dob'];
+$nationality = $_POST['nationality'];
+
+$sql = "INSERT INTO studenti (nome, cognome, dataNascita, nazionalita) VALUES (?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+
+if (!$stmt) {
+    die("Errore prepare(): " . $conn->error);
+}
+ 
+$stmt->bind_param("ssss", $name, $surname, $dob, $nationality);
+
+if ($stmt->execute()) {
+    echo "Dati inseriti con successo.";
+} else {
+    echo "Errore execute(): " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
